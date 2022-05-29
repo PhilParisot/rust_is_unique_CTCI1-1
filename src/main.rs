cc517afe3cac309ceb6dc3253c8ef93d0a49be43
Philippe Parisot <pparisot3@gmail.com>
2022-05-29 13:17:00 -0400

    HashSet check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let mut uniqueChars: HashSet<char> = HashSet::new();
+    let string = "abcdefg";
+    println!("{}", string);
+
+    for c in string.chars() {
+        if !uniqueChars.contains(&c) {
+            uniqueChars.insert(c);
+        } else {
+            println!("String is not unique");
+            return;
+        }
+    }
+    println!("String is unique");
 }
